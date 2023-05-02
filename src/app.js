67fb1892983eddc2f7b77748912f155e48038fe0
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-05-02 12:44:05 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
+import './styles/styles.scss';
+import '../node_modules/normalize.css'
+
+
+ReactDOM.render(<IndecisionApp/>,document.getElementById('app')); 
